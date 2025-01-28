95f5495eeead11207da55de0f17f12a86d3eba5b
peterpetrov123 <peterpetrov123@gmail.com>
2025-01-28 17:12:49 +0000

    fixed yml file erros and updated init.sql to include all files

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,3 +1,4 @@
+-- Create Database
 CREATE DATABASE IF NOT EXISTS movielens;
 USE movielens;
 
@@ -8,10 +9,57 @@ CREATE TABLE IF NOT EXISTS movies (
     genres VARCHAR(255)
 );
 
--- Load Movies Data (with duplicate handling)
+-- Create Links Table
+CREATE TABLE IF NOT EXISTS links (
+    movieId INT PRIMARY KEY,
+    imdbId INT,
+    tmdbId INT
+);
+
+-- Create Ratings Table
+CREATE TABLE IF NOT EXISTS ratings (
+    userId INT,
+    movieId INT,
+    rating FLOAT,
+    timestamp BIGINT
+);
+
+-- Create Tags Table
+CREATE TABLE IF NOT EXISTS tags (
+    userId INT,
+    movieId INT,
+    tag VARCHAR(255),
+    timestamp BIGINT
+);
+
+-- Load Movies Data
 LOAD DATA INFILE '/var/lib/mysql-files/movies.csv'
-IGNORE INTO TABLE movies
+INTO TABLE movies
 FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+-- Load Links Data
+LOAD DATA INFILE '/var/lib/mysql-files/links.csv'
+INTO TABLE links
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+-- Load Ratings Data
+LOAD DATA INFILE '/var/lib/mysql-files/ratings.csv'
+INTO TABLE ratings
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+-- Load Tags Data
+LOAD DATA INFILE '/var/lib/mysql-files/tags.csv'
+INTO TABLE tags
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
