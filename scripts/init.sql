a144e44b4c7b024e974aaa69a16997e1ff2996d7
panos03 <panos.haritsis@gmail.com>
2025-01-28 14:43:48 +0000

    move csv

--- scripts/init.sql
+++ scripts/init.sql
@@ -2,12 +2,12 @@
 CREATE DATABASE IF NOT EXISTS movielens;
 USE movielens;
 
--- Create Movies Table
-CREATE TABLE IF NOT EXISTS movies (
-    movieId INT PRIMARY KEY,
-    title VARCHAR(255),
-    genres VARCHAR(255)
-);
+-- -- Create Movies Table
+-- CREATE TABLE IF NOT EXISTS movies (
+--     movieId INT PRIMARY KEY,
+--     title VARCHAR(255),
+--     genres VARCHAR(255)
+-- );
 
 -- Create Movies Table
 CREATE TABLE IF NOT EXISTS hello (
@@ -16,14 +16,14 @@ CREATE TABLE IF NOT EXISTS hello (
     c VARCHAR(255)
 );
 
-INSERT INTO movies (movieId, title, genres) VALUES (1, 'Alice', 'l');
--- Load Movies Data
-SET GLOBAL local_infile = 1;
-LOAD DATA LOCAL INFILE 'var/lib/mysql-files/movies.csv'
-INTO TABLE movies
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"' 
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+-- INSERT INTO movies (movieId, title, genres) VALUES (1, 'Alice', 'l');
+-- -- Load Movies Data
+-- --SET GLOBAL local_infile = 1;
+-- LOAD DATA LOCAL INFILE '/var/lib/mysql/movies.csv'
+-- INTO TABLE movies
+-- FIELDS TERMINATED BY ',' 
+-- ENCLOSED BY '"' 
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 ROWS;
 
--- Add other tables and load data similarly.
+-- -- Add other tables and load data similarly.
