4ac27818d6d942a3d3ce7ac61325aa4320b23856
peterpetrov123 <peterpetrov123@gmail.com>
2025-03-17 16:50:20 +0000

    addded unique id to genres_movies.csv and languages_movies.csv

--- preprocessing/normalization.py
+++ preprocessing/normalization.py
@@ -26,7 +26,7 @@ if "directors" in movies.columns:
     movies.drop(columns=['directors'], inplace=True)
 
 
-# Normalize genres (with ID first)
+# Normalize genres (with unique ID)
 if "genres" in movies.columns:
     genres_movies = movies[['movieId', 'genres']].dropna()
     genres_movies = genres_movies.assign(genres=genres_movies['genres'].str.split('|')).explode('genres')
@@ -35,9 +35,10 @@ if "genres" in movies.columns:
     unique_genres = genres_movies[['genres']].drop_duplicates().reset_index(drop=True)
     unique_genres['genreId'] = range(1, len(unique_genres) + 1)
 
-    # Merge genreId into genres_movies
+    # Merge genreId into genres_movies and add uniqueId
     genres_movies = genres_movies.merge(unique_genres, on="genres", how="left")
-    genres_movies = genres_movies[['movieId', 'genreId']]
+    genres_movies = genres_movies[['movieId', 'genreId']].reset_index(drop=True)
+    genres_movies.insert(0, 'uniqueId', genres_movies.index + 1)  # Add uniqueId
 
     # Save files (with genreId first)
     unique_genres = unique_genres[['genreId', 'genres']]
@@ -45,7 +46,7 @@ if "genres" in movies.columns:
     genres_movies.to_csv(os.path.join(output_dir, "genres_movies.csv"), index=False)
     movies.drop(columns=['genres'], inplace=True)
 
-# Normalize languages (with ID first)
+# Normalize languages (with unique ID)
 if "language" in movies.columns:
     languages_movies = movies[['movieId', 'language']].dropna()
     languages_movies = languages_movies.assign(language=languages_movies['language'].str.split('|')).explode('language')
@@ -54,9 +55,10 @@ if "language" in movies.columns:
     unique_languages = languages_movies[['language']].drop_duplicates().reset_index(drop=True)
     unique_languages['languageId'] = range(1, len(unique_languages) + 1)
 
-    # Merge languageId into languages_movies
+    # Merge languageId into languages_movies and add uniqueId
     languages_movies = languages_movies.merge(unique_languages, on="language", how="left")
-    languages_movies = languages_movies[['movieId', 'languageId']]
+    languages_movies = languages_movies[['movieId', 'languageId']].reset_index(drop=True)
+    languages_movies.insert(0, 'uniqueId', languages_movies.index + 1)  # Add uniqueId
 
     # Save files (with languageId first)
     unique_languages = unique_languages[['languageId', 'language']]
